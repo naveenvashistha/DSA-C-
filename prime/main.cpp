d2cebfead8ec288f473ef4caa3bc20e3ef281fcc
Naveen Vashistha <naveenvashistha2000@gmail.com>
2024-09-05 01:43:13 +0530

    Q-next

---
+++ prime/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isPrime(int N);
+
+int main()
+{
+    int N;
+    cin >> N;
+    cout << isPrime(N) << endl; 
+}
+
+bool isPrime(int n)
+{
+	// Write your code here.
+    if (n == 1) return false;
+	for(int i = 2; i <= (int)sqrt(n); i++)
+	{
+		if (n % i == 0)
+		{
+			return false;		
+		}
+	}
+	return true;
+}
