c205eee77e4eecddd6ea2b6ae7869e7815243680
Naveen Vashistha <naveenvashistha2000@gmail.com>
2024-09-06 16:41:04 +0530

    Q-next

--- Recursion - print 1 to n/main.cpp
+++ Recursion - print 1 to n/main.cpp
@@ -2,19 +2,30 @@
 
 using namespace std;
 
-void printNos(int n);
+void printNos1(int n);
+void printNos2(int n);
 
 int main()
 {
     int N;
     cin >> N;
-    printNos(N);
+    printNos1(N);
+	cout << endl;
+	printNos2(N);
 }
 
-void printNos(int N)
+void printNos1(int N)
 {
 	//Your code here
 	if (N > 1)
-		printNos(N - 1);
+		printNos1(N - 1);
 	cout << N << " ";
+}
+
+void printNos2(int N)
+{
+	//Your code here
+	cout << N << " ";
+	if (N > 1)
+		printNos2(N - 1);
 }
