b932c1c6553daa1b600f29900fb8b123f144b9c4
Naveen Vashistha <naveenvashistha2000@gmail.com>
2025-01-07 02:38:11 +0530

    Q-next

--- Recursion/atoi/main.cpp
+++ Recursion/atoi/main.cpp
@@ -2,55 +2,50 @@
 
 using namespace std;
 
-int myAtoi(string s);
-long long atoi(string s, int i, long long ans, bool isNegative);
+double myPow(double x, int n);
 
 int main()
 {
-    string s = "  +  413";
-    int ss = myAtoi(s);
-    cout << ss << endl;
+   cout << myPow(2.0, 10) << endl;
 }
 
-long long atoi(string s, int i, long long ans, bool isNegative)
-{
-    if(s[i] >= '0' && s[i] <= '9')
+//brute
+//time:O(n)
+//space: O(1)
+double myPow(double x, int n) {
+    long m;
+    if(n < 0) m = -(long)n;
+    else m = n;
+    double ans = 1;
+    for(long i = 1; i <= m; i++)
     {
-        ans = ans * 10 + (s[i] - '0');
-        if(isNegative == true)
+        ans = ans * x;
+    }
+    if(n < 0) return 1 / ans;
+    return ans;
+}
+
+//orgasmic (binary exponentiation)
+//time: O(logn)
+//space: O(1)
+double myPow(double x, int n) {
+    long nn;
+    if(n < 0) nn = -(long)n;
+    else nn = n;
+    double ans = 1;
+    while(nn)
+    {
+        if(nn % 2)
         {
-            if(-ans <= INT_MIN)
-                return INT_MIN;
+            ans = ans * x;
+            nn = nn - 1;
         }
-        else if(ans >= INT_MAX) return INT_MAX;
-        return atoi(s, i + 1, ans, isNegative);
+        else
+        {
+            x = x * x;
+            nn = nn / 2;
+        }
     }
-    else
-    {
-        if(isNegative == true) return -ans;
-        return ans;
-    }
-}
-
-//orgasmic
-//time: O(n)
-//space: O(n)
-int myAtoi(string s) {
-    int i = 0;
-    long long ans = 0;
-    bool isNegative = false;
-
-    //handling leading white space
-    while(s[i] == ' ') i++;
-
-    //handling sign
-    if(s[i] == '-')
-    {
-        isNegative = true;
-        i++;
-    }
-    else if(s[i] == '+') i++;
-
-    ans = atoi(s, i, ans, isNegative);
-    return (int)ans;
+    if(n < 0) return 1 / ans;
+    return ans;
 }
