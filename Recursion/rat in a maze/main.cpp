fb7dc057639d127731dc6f239c961278771675fb
Naveen Vashistha <naveenvashistha2000@gmail.com>
2025-01-17 00:11:06 +0530

    Q-next

--- Recursion/rat in a maze/main.cpp
+++ Recursion/rat in a maze/main.cpp
@@ -2,18 +2,22 @@
 
 using namespace std;
 
-bool findWord(vector<vector<char>>& board, string word, int ind, int row, int column);
-bool exist(vector<vector<char>>& board, string word);
+vector<string> findPath(vector<vector<int>> &mat) ;
+void findPaths(vector<vector<int>> &mat, vector<string> &ans, string path, int row, int col, int n);
 
 int main()
 {
-    vector<vector<char>> board = {{'A', 'B', 'C'}, {'B', 'C', 'A'}};
-    string word = "ABB";
-    cout << exist(board, word) << endl;
+    vector<vector<int>> mat = {{1, 0, 1}, {1, 0, 1}, {1, 1, 1}};
+    string path = "";
+    vector<string> out = findPath(mat);
+    for(auto i: out)
+    {
+        for(auto j: i)
+            cout << j << " ";
+        cout << endl;
+    }
 }
-//orgasmic
-//time: O(n*n*4^k) where k is the length of word
-//space: O(k) recursive space
+
 void findPath(vector<vector<int>> &mat, vector<string> &ans, string path, int row, int col, int n)
 {
     if(row == n - 1 && col == n - 1)
@@ -26,10 +30,10 @@ void findPath(vector<vector<int>> &mat, vector<string> &ans, string path, int ro
     int pos = mat[row][col];
     mat[row][col] = 0;
     
-    findPath(mat, ans, path + "D", row + 1, col, n);
-    findPath(mat, ans, path + "R", row, col + 1, n);
-    findPath(mat, ans, path + "U", row - 1, col, n);
-    findPath(mat, ans, path + "L", row, col - 1, n);
+    findPaths(mat, ans, path + "D", row + 1, col, n);
+    findPaths(mat, ans, path + "R", row, col + 1, n);
+    findPaths(mat, ans, path + "U", row - 1, col, n);
+    findPaths(mat, ans, path + "L", row, col - 1, n);
     
     mat[row][col] = pos;
 }
@@ -43,7 +47,7 @@ vector<string> findPath(vector<vector<int>> &mat) {
     vector<string> ans;
     string path = "";
     if(mat[0][0] == 0 || mat[n - 1][n - 1] == 0) return ans;
-    findPath(mat, ans, path, 0, 0, n);
+    findPaths(mat, ans, path, 0, 0, n);
     sort(ans.begin(), ans.end());
     return ans;
 }
