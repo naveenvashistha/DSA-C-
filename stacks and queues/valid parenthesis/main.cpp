a1fd42e598e57904a1ccc17f9ec1cba67efa2836
Naveen Vashistha <naveenvashistha2000@gmail.com>
2025-01-28 18:49:37 +0530

    Q-next

---
+++ stacks and queues/valid parenthesis/main.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isValid(string s) {
+    stack<int> st;
+    int i = 0;
+    while(s[i] != '\0')
+    {
+        if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+            st.push(s[i]);
+        else
+        {
+            if(st.empty()) return false;
+            if(s[i] == ')')
+            {
+                if(st.top() != '(') return false;
+            }
+            else if(s[i] == '}')
+            {
+                if(st.top() != '{') return false;
+            }
+            else
+            {
+                if(st.top() != '[') return false;
+            }
+            st.pop();
+        }
+        i++;
+    }
+    if(st.empty()) return true;
+    return false;
+}
+
+int main()
+{
+	cout << isValid("(())") << endl;
+}
