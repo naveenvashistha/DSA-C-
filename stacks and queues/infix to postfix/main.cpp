3f4b5d91a07e607bf8c6af997b5b3f2b47aa101d
Naveen Vashistha <naveenvashistha2000@gmail.com>
2025-01-29 22:44:18 +0530

    Q-next

---
+++ stacks and queues/infix to postfix/main.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// C++ implementation to convert infix expression to postfix
+#include <bits/stdc++.h>
+using namespace std;
+
+//orgasmic
+//time: O(n)
+//space: O(n)
+class Solution {
+  public:
+    // Function to convert an infix expression to a postfix expression.
+    string infixToPostfix(string& s) {
+        // Your code here
+        string ans = "";
+        unordered_map<char, int> op = {
+          {'^', 5},
+          {'*', 4},
+          {'/', 4},
+          {'+', 3},
+          {'-', 3}
+        };
+        stack<char> st;
+        for(auto i: s)
+        {
+            if(isalpha(i) || isdigit(i)) ans.push_back(i);
+            else if(i == '(') st.push(i);
+            else if(i == ')')
+            {
+                while(st.top() != '(')
+                {
+                    char c = st.top();
+                    ans.push_back(c);
+                    st.pop();
+                }
+                st.pop();
+            }
+            else
+            {
+                while(!st.empty() && op[i] <= op[st.top()])
+                {
+                    char c = st.top();
+                    ans.push_back(c);
+                    st.pop();
+                }
+                st.push(i);
+            }
+        }
+        while(!st.empty())
+        {
+            char c = st.top();
+            ans.push_back(c);
+            st.pop();
+        }
+        return ans;
+    }
+};
+
+
+// } Driver Code Ends
+int main()
+{
+	Solution s;
+    string ss = "a+b";
+    cout << s.infixToPostfix(ss) << endl;
+}
