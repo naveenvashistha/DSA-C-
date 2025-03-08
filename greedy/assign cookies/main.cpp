51a5b4664f1f603736cf115e8a1db47e0e7d9d3d
Naveen Vashistha <naveenvashistha2000@gmail.com>
2025-03-09 02:16:42 +0530

    Q-next

---
+++ greedy/assign cookies/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//time: O(nlogn)
+//space: O(n)
+int findContentChildren(vector<int>& g, vector<int>& s) {
+    sort(g.begin(), g.end());
+    sort(s.begin(), s.end());
+    int i = 0, j = 0;
+    while(i < g.size() && j < s.size())
+    {
+        if(g[i] <= s[j])
+        {
+            i++;
+            j++;
+        }
+        else j++;
+    }
+    return i;
+}
+
+int main()
+{
+}
+
