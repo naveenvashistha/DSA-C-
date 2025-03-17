aceccf7b8c26253cc8093ea7b220f236deb2bb7b
Naveen Vashistha <naveenvashistha2000@gmail.com>
2025-03-17 23:46:43 +0530

    Q-next

--- Binary trees/height/main.cpp
+++ Binary trees/height/main.cpp
@@ -13,6 +13,7 @@ public:
     TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
 };
 
+//my method
 //time: O(n)
 //space: O(n)
 int height(TreeNode* node, int h)
@@ -21,6 +22,17 @@ int height(TreeNode* node, int h)
     int h1 = height(node->left, h + 1);
     int h2 = height(node->right, h + 1);
     return max(h1, h2);
+}
+
+//strivers
+//time: O(n)
+//space: O(n)
+int height1(TreeNode* node)
+{
+    if(node == NULL) return 0;
+    int h1 = height1(node->left);
+    int h2 = height1(node->right);
+    return 1 + max(h1, h2);
 }
 int maxDepth(TreeNode* root) {
     int ans = height(root, 0);
