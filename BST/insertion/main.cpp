33706fd4e65384f5af46dec226cade397932d24b
Naveen Vashistha <naveenvashistha2000@gmail.com>
2025-03-29 19:27:15 +0530

    Q-next

--- BST/insertion/main.cpp
+++ BST/insertion/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 
-
+//recursive
 //time: O(n)
 //space: O(h)
 TreeNode* insertIntoBST(TreeNode* root, int val) {
@@ -12,4 +12,30 @@ TreeNode* insertIntoBST(TreeNode* root, int val) {
     if(val > root->val) root->right = insertIntoBST(root->right, val);
     else root->left = insertIntoBST(root->left, val);
     return root;
+}
+
+//iterative
+//time: O(n)
+//space: O(1)
+TreeNode* insertIntoBST(TreeNode* root, int val) {
+    if(root == NULL) return new TreeNode(val);
+    TreeNode* rooty = root;
+    while(true)
+    {
+        if(val > root->val){
+            if(root->right != NULL) root = root->right;
+            else{
+                root->right = new TreeNode(val);
+                break;
+            }
+        }
+        else{
+            if(root->left != NULL) root = root->left;
+            else{
+                root->left = new TreeNode(val);
+                break;
+            }
+        }
+    }
+    return rooty;
 }
