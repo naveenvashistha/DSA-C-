c22a3ebb469b00dedf97668445b1ded3f6ffdb82
Naveen Vashistha <naveenvashistha2000@gmail.com>
2025-04-15 01:26:11 +0530

    Q-next

--- graphs/course scheduler II/main.cpp
+++ graphs/course scheduler II/main.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+//using dfs
 //time: O(v + e)
 //space: O(v)
 bool dfs(int node, vector<vector<int>>& adj, vector<int>& visited, vector<int> pathvis, stack<int>& st)
@@ -38,5 +39,36 @@ vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         ans.push_back(st.top());
         st.pop();
     }
+    return ans;
+}
+
+//using bfs
+//time: O(v + e)
+//space: O(v)
+vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<int> ans;
+    vector<vector<int>> adj(numCourses);
+    vector<int> indegree(numCourses);
+    queue<int> q;
+    for(auto i: prerequisites) adj[i[1]].push_back(i[0]);
+    for(auto i: adj)
+    {
+        for(auto j: i) indegree[j]++;
+    }
+    for(int i = 0; i < numCourses; i++){
+        if(indegree[i] == 0) q.push(i);
+    }
+    while(!q.empty())
+    {
+        int node = q.front();
+        ans.push_back(node);
+        q.pop();
+        for(auto i: adj[node])
+        {
+            indegree[i]--;
+            if(indegree[i] == 0) q.push(i);
+        }
+    }
+    if(ans.size() < numCourses) return {};
     return ans;
 }
