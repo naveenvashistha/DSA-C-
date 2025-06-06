45564b8b5a52e6a08691ebd0aea4e4128f50025c
Naveen Vashistha <naveenvashistha2000@gmail.com>
2025-06-06 14:11:15 +0530

    Q-next

--- rough/main.cpp
+++ rough/main.cpp
@@ -1,19 +1,39 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+
 int main() {
-    try {
-        cout << "Inside try block." << endl;
-        int x = 10, y = 0;
-		if(y == 0) throw "hi";
-        int z = x / y;  // Division by zero, ERROR
-        cout << "Result: " << z << endl;
+    int n, m;
+    cin >> n >> m;
+    int u, v, w;
+    vector<vector<pair<int, int>>> adj(n + 1);
+    for(int i = 1; i <= m; i++)
+    {
+        cin >> u >> v >> w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
     }
-    catch (const char* e) {
-        cout << "Caught exception: " << e << endl;
+    vector<int> dist(n + 1, INT_MAX);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, 1});
+    dist[1] = 0;
+    while(!pq.empty())
+    {
+        int orr = pq.top().first;
+        int node = pq.top().second;
+        cout << orr << " " << node << endl;
+        pq.pop();
+        if(node == n){
+            cout << dist[n] << endl;
+            return 0;
+        }
+        for(auto i: adj[node])
+        {
+            int ans = orr | i.second;
+            if(ans < dist[i.first]){
+                dist[i.first] = orr | i.second;
+                pq.push({dist[i.first], i.first});
+            }
+        }
     }
-
-    cout << "After try-catch block." << endl;
-
-    return 0;
 }
